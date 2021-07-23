32ea5f5c4a31ce40680649c3b70b3b181a0f23a4
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-07-24 04:01:27 +0430

    Update BASE_DIR

--- project/settings/_base.py
+++ project/settings/_base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
