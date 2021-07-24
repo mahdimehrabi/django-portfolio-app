ca6f3cc5bf88dd01049730971878939f229cdfbf
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-07-25 02:52:33 +0430

    Fix missing comma

--- project/settings/_base.py
+++ project/settings/_base.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'project.apps.portfolio.apps.PortfolioConfig'
+    'project.apps.portfolio.apps.PortfolioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
