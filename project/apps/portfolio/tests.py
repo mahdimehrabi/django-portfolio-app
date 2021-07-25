8693f3be079b859b78b3aef5c9405d6ad0fe308e
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-07-26 00:49:07 +0430

    Added base test for index

--- project/apps/portfolio/tests.py
+++ project/apps/portfolio/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.shortcuts import reverse
+from .models import Menu
 # Create your tests here.
+
+
+class MenuTest(TestCase):
+    def SetUp(self):
+        self.client = Client()
+
+    def test_index_page(self):
+        menu = Menu.objects.create(
+            title="menu1", link="https://www.google.com")
+        response = self.client.get(reverse('portfolio:index'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, menu.title)
