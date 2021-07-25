957ff8d6c37e5e0342fb40078d4af415cf891568
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-07-26 02:51:51 +0430

    Update settings for languages

--- project/settings/_base.py
+++ project/settings/_base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -115,8 +117,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-LOCAL_PATHS = [
+LOCALE_PATHS = [
     BASE_DIR / 'locale'
+]
+LANGUAGES = [
+    ('fa', 'fa'),
+    ('en', 'en'),
 ]
 
 # Static files (CSS, JavaScript, Images)
