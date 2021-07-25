2906e1e0e29e1673b8286d88afbd064c236d7026
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-07-26 00:41:00 +0430

    Added Menu model

--- project/apps/portfolio/models.py
+++ project/apps/portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=35)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.title
