42e3a53ab477fa474bdf2ca2e66b1cff22ee517a
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-07-26 02:09:33 +0430

    Added language base settings

--- project/settings/_base.py
+++ project/settings/_base.py
@@ -115,6 +115,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+LOCAL_PATHS = [
+    BASE_DIR / 'locale'
+]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
