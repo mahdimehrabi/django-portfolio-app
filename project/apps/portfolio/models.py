1fa6128d1bfc1509341ab3ade4e328d9fa8ee609
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-07-27 01:24:09 +0430

    Added persian title to menu model

--- project/apps/portfolio/models.py
+++ project/apps/portfolio/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Menu(models.Model):
     title = models.CharField(max_length=35)
+    title_fa = models.CharField('title persian', max_length=35)
     link = models.URLField()
 
     def __str__(self):
