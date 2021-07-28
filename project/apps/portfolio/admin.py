a7dedca7b7d458b0ce2cb06c3065db23fa013eb4
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-07-28 21:17:58 +0430

    Disable Header and Menu delete in admin

--- project/apps/portfolio/admin.py
+++ project/apps/portfolio/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
-from .models import Menu
+from .models import Menu, HeaderButton, Header
 
 # Register your models here.
+
+
+class HeaderButtonInline(admin.TabularInline):
+    model = HeaderButton
+    extra = 1
 
 
 class MenuAdmin(admin.ModelAdmin):
     list_display = ('title', 'title_fa')
 
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class HeaderAdmin(admin.ModelAdmin):
+    list_display = ('first_title', 'second_title')
+    inlines = [HeaderButtonInline]
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 admin.site.register(Menu, MenuAdmin)
+admin.site.register(Header, HeaderAdmin)
