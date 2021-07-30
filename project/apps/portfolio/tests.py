13cdc4e1a670a6d15e1335f1e9a4e20796e52c39
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-07-31 04:25:28 +0430

    Added header model to index page + test

--- project/apps/portfolio/tests.py
+++ project/apps/portfolio/tests.py
@@ -1,17 +1,31 @@
 from django.test import TestCase, Client
+from django.utils.translation import activate
 from django.shortcuts import reverse
-from .models import Menu
+from .models import Menu, Header
 # Create your tests here.
 
 
-class MenuTest(TestCase):
+class IndexTest(TestCase):
     def SetUp(self):
         self.client = Client()
 
-    def test_index_page(self):
+    def test_display_menu_index_page(self):
         menu = Menu.objects.create(
             title="menu1", link="https://www.google.com")
-        response = self.client.get(reverse('portfolio:index'))
 
+        activate('en')
+        response = self.client.get(reverse('portfolio:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, menu.title)
+
+    def test_display_header_index_page(self):
+        header = Header.objects.create(
+            first_title='gdsgds', first_title_fa='sdg',
+            second_title="Gdssgd", second_title_fa='gdgsgsdgdsc',
+            background='images/background.jpg')
+
+        activate('en')
+        response = self.client.get(reverse('portfolio:index'))
+        self.assertContains(response, header.first_title)
+        self.assertContains(response, header.second_title)
+        self.assertContains(response, header.background)
--- project/apps/portfolio/views.py
+++ project/apps/portfolio/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import Menu
+from .models import Menu, Header
 
 # Create your views here.
 
 
 def index(request):
     menus = Menu.objects.all()
+    header = Header.objects.first()
     context = {
-        'menus': menus
+        'menus': menus,
+        'header': header,
     }
     return render(request, 'portfolio/index.html', context)
