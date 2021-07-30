5bd62af108f549e82463dd950142e0c7c0c4796c
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-07-31 04:11:57 +0430

    Added media upload configs

--- project/settings/_base.py
+++ project/settings/_base.py
@@ -134,3 +134,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- project/urls.py
+++ project/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += i18n_patterns(
     path('', include('project.apps.portfolio.urls', namespace='portfolio'))
