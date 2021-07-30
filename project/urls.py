10760597a0b46ef9788d8d6274554b560d46b650
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-07-31 03:05:42 +0430

    Added translation urls

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('project.apps.portfolio.urls'))
 ]
+
+urlpatterns += i18n_patterns(
+    path('', include('project.apps.portfolio.urls'))
+)
