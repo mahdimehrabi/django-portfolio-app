c1e764b57a2d4316bead462ef174d5169cc9adaf
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-07-31 04:34:55 +0430

    Added more tests in header and menu test
    Added forgotted tests about showing translates header titles and menu title in second language

--- project/apps/portfolio/tests.py
+++ project/apps/portfolio/tests.py
@@ -4,6 +4,10 @@ from django.shortcuts import reverse
 from .models import Menu, Header
 # Create your tests here.
 
+# the code of second language that you used for your website
+# in my case is fa
+second_lang = 'fa'
+
 
 class IndexTest(TestCase):
     def SetUp(self):
@@ -11,12 +15,16 @@ class IndexTest(TestCase):
 
     def test_display_menu_index_page(self):
         menu = Menu.objects.create(
-            title="menu1", link="https://www.google.com")
+            title="menu1", link="https://www.google.com", title_fa='sdsgdsdg')
 
         activate('en')
         response = self.client.get(reverse('portfolio:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, menu.title)
+
+        activate(second_lang)
+        response = self.client.get(reverse('portfolio:index'))
+        self.assertContains(response, menu.title_fa)
 
     def test_display_header_index_page(self):
         header = Header.objects.create(
@@ -29,3 +37,8 @@ class IndexTest(TestCase):
         self.assertContains(response, header.first_title)
         self.assertContains(response, header.second_title)
         self.assertContains(response, header.background)
+
+        activate(second_lang)
+        response = self.client.get(reverse('portfolio:index'))
+        self.assertContains(response, header.first_title_fa)
+        self.assertContains(response, header.second_title_fa)
