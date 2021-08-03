3dbf0cbcc7d12ba1a06047eabe697f1c75aade9e
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-08-04 02:13:24 +0430

    Fixed default value of DEBUG settings

--- project/settings/_base.py
+++ project/settings/_base.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get('DEBUG', 'DEBUG'))
+DEBUG = int(os.environ.get('DEBUG', 0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
