6a9af9dbd9978f2eae157d27bac0487e21862fae
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-08-11 01:46:39 +0430

    Update _base settings
    Added default allowed hosts

--- project/settings/_base.py
+++ project/settings/_base.py
@@ -28,7 +28,8 @@ DEBUG = int(os.environ.get('DEBUG', 1))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', ' ').split(' ')
+ALLOWED_HOSTS = os.environ.get(
+    'DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1 [::1]').split(' ')
 
 
 # Application definition
