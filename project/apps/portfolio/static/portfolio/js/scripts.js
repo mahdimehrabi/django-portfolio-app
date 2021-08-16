4ed2237490f3f126ff51f005746d519d155ef757
mahdi mehrabi <mahdimehrabi17@gmail.com>
2021-08-16 21:38:21 +0430

    Added javascripts of showing success messages

--- project/apps/portfolio/static/portfolio/js/scripts.js
+++ project/apps/portfolio/static/portfolio/js/scripts.js
@@ -8,90 +8,110 @@
 
     Description: This file contains all the scripts associated with the single-page
     portfolio website.
-*/
+    */
 
-(function($) {
+(function ($) {
+  // Remove no-js class
+  $("html").removeClass("no-js");
 
-    // Remove no-js class
-    $('html').removeClass('no-js');
+  // Animate to section when nav is clicked
+  $("header a").click(function (e) {
+    // Treat as normal link if no-scroll class
+    if ($(this).hasClass("no-scroll")) return;
 
-    // Animate to section when nav is clicked
-    $('header a').click(function(e) {
+    e.preventDefault();
+    var heading = $(this).attr("href");
+    var scrollDistance = $(heading).offset().top;
 
-        // Treat as normal link if no-scroll class
-        if ($(this).hasClass('no-scroll')) return;
+    $("html, body").animate(
+      {
+        scrollTop: scrollDistance + "px",
+      },
+      Math.abs(window.pageYOffset - $(heading).offset().top) / 1
+    );
 
-        e.preventDefault();
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
+    // Hide the menu once clicked if mobile
+    if ($("header").hasClass("active")) {
+      $("header, body").removeClass("active");
+    }
+  });
 
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+  // Scroll to top
+  $("#to-top").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+  });
 
-        // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
-        }
+  // Scroll to first element
+  $("#lead-down span").click(function () {
+    var scrollDistance = $("#lead").next().offset().top;
+    $("html, body").animate(
+      {
+        scrollTop: scrollDistance + "px",
+      },
+      500
+    );
+  });
+
+  // Create timeline
+  $("#experience-timeline").each(function () {
+    $this = $(this); // Store reference to this
+    $userContent = $this.children("div"); // user content
+
+    // Create each timeline block
+    $userContent.each(function () {
+      $(this)
+        .addClass("vtimeline-content")
+        .wrap(
+          '<div class="vtimeline-point"><div class="vtimeline-block"></div></div>'
+        );
     });
 
-    // Scroll to top
-    $('#to-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
+    // Add icons to each block
+    $this.find(".vtimeline-point").each(function () {
+      $(this).prepend(
+        '<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>'
+      );
     });
 
-    // Scroll to first element
-    $('#lead-down span').click(function() {
-        var scrollDistance = $('#lead').next().offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, 500);
+    // Add dates to the timeline if exists
+    $this.find(".vtimeline-content").each(function () {
+      var date = $(this).data("date");
+      if (date) {
+        // Prepend if exists
+        $(this)
+          .parent()
+          .prepend('<span class="vtimeline-date">' + date + "</span>");
+      }
     });
+  });
 
-    // Create timeline
-    $('#experience-timeline').each(function() {
+  // Open mobile menu
+  $("#mobile-menu-open").click(function () {
+    $("header, body").addClass("active");
+  });
 
-        $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
+  // Close mobile menu
+  $("#mobile-menu-close").click(function () {
+    $("header, body").removeClass("active");
+  });
 
-        // Create each timeline block
-        $userContent.each(function() {
-            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
-        });
-
-        // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
-        });
-
-        // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
-            var date = $(this).data('date');
-            if (date) { // Prepend if exists
-                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
-            }
-        });
-
+  // Load additional projects
+  $("#view-more-projects").click(function (e) {
+    e.preventDefault();
+    $(this).fadeOut(300, function () {
+      $("#more-projects").fadeIn(300);
     });
+  });
 
-    // Open mobile menu
-    $('#mobile-menu-open').click(function() {
-        $('header, body').addClass('active');
-    });
-
-    // Close mobile menu
-    $('#mobile-menu-close').click(function() {
-        $('header, body').removeClass('active');
-    });
-
-    // Load additional projects
-    $('#view-more-projects').click(function(e){
-        e.preventDefault();
-        $(this).fadeOut(300, function() {
-            $('#more-projects').fadeIn(300);
-        });
-    });
-
+  // show alert messages
+  $(document).ready(function () {
+    if ($(".alert.success").length) {
+      Swal.fire($(".alert.success").text(), "", "success");
+    }
+  });
 })(jQuery);
